033ea356a835f828b4a61278830eca9fb4d7296b
devjames <devjames@jamess-mbp.home>
2018-07-19 21:30:18 +0100

    fml more cors2

--- fagnode/app.js
+++ fagnode/app.js
@@ -6,21 +6,24 @@ app.use(bodyParser.urlencoded({'extended' : 'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type : 'application/vnd.api+json'}));
 
-app.use((req, res, next) => {
-  const origin = req.get('origin');
+// Add headers
+app.use(function (req, res, next) {
 
-  // TODO Add origin validation
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-  // intercept OPTIONS method
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(204);
-  } else {
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
-  }
 });
 
 var multer = require('multer');
