18be03a295343c80d6f0cdf3f5f02a1040c13e40
devjames <devjames@jamess-mbp.home>
2018-07-19 23:55:46 +0100

    cors and options requests make ME want to commit 13

--- fagnode/app.js
+++ fagnode/app.js
@@ -126,8 +126,10 @@ app.get('/api/outfits', function(req, res){
 
 //delivers a number of unapproved outfits, defaulting to 6
 app.get('/api/outfits/unapproved', function(req, res){
+	console.log('trig1')
 	let limit = parseInt(req.body.limit) || 6;
 	Outfit.find({ accepted: false }).limit(limit).lean().exec(function(err, outfit){
+			console.log('trig2')
 		if (err) return console.log(err);
 		console.log(convertImageData(outfit));
 		res.json({ test: 'test'});
