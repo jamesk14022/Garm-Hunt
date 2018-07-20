75951853e61782727f1948ab921f7701161111d8
devjames <devjames@jamess-mbp.home>
2018-07-20 12:08:05 +0100

    typo10

--- fagnode/app.js
+++ fagnode/app.js
@@ -117,7 +117,7 @@ app.get('/api/outfits', function(req, res){
 
 //delivers a number of unapproved outfits, defaulting to 6
 app.get('/api/unapproved/outfits', function(req, res){
-	Outfit.find({ accepted: false }).limit(limit).lean().exec(function(err, outfit){
+	Outfit.find({ accepted: false }).lean().exec(function(err, outfit){
 		if (err) return console.log(err);
 		res.json(convertImageData(outfit));
 	}
