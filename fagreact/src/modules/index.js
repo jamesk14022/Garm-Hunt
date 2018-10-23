e7f9f7d2f70edbf355d7bb3ea16e95c88517fb59
devjames <devjames@jamess-mbp.home>
2018-10-23 22:28:03 +0100

    Cleanup

--- fagreact/src/modules/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-import notification from './notification';
-
-export default combineReducers({
-  routing: routerReducer,
-  notification
-});
